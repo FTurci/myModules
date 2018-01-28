3da45f8b72877ab27eeabdf5d3bd17b0196a0496
FTurci <turci@me.com>
2018-01-28 18:47:16 +0000

    Square Well test

--- ao-to-morse.py
+++ ao-to-morse.py
@@ -1,25 +1,26 @@
 import pylab as pl
+
 import numpy as np
 def reduced_second_virial(B2, sigma_eff):
 	return B2/(2./3.*pl.pi*sigma_eff**2)
 
-def second_virial(potential,params, beta,rmax=100, npoints=1e5):
-	from scipy.integrate import cumtrapz
+def second_virial(potential,params, beta,rmax=20.0, npoints=10000):
+	from scipy.integrate import simps
 	r = pl.linspace(0,rmax,npoints)
 	y = r**2*(1.-pl.exp(-beta*potential(r,params=params)))
-	I = cumtrapz(y,r,initial=0)
-	return I[-1]
+	I = simps(y,r)*2*pl.pi #don't forget the 2pi!
+	return I
 
-def effective_sigma(potential,params, beta,rmax=100, npoints=1e5):
-	from scipy.integrate import cumtrapz
+def effective_sigma(potential,params, beta,rmax=20.0, npoints=10000):
+	from scipy.integrate import simps
 	r = np.linspace(0,rmax,npoints)
 	p = potential(r, params=params)
 	repulsive  = pl.zeros(len(r))
 	repulsive [p>0] = p[p>0]
 
 	y = 1.-pl.exp(-beta*repulsive)
-	I = cumtrapz(y,r,initial=0)
-	return I[-1]
+	I = simps(y,r)
+	return I
 
 def morse(r,params=[1.,1.,1.],):
 	rho ,sigma, epsilon= params[0],params[1], params[2]
@@ -37,6 +38,7 @@ def ao_potential(r, params=[1.,1.,1]):
 	q, sigma,etap = params[0], params[1],params[2]
 
 	Rg = sigma*q/2.
+	# print ("Rg", Rg)
 	zp = 6.*etap/(pl.pi*(2*Rg)**3)
 
 	rhard = r<= sigma
@@ -53,30 +55,38 @@ def ao_contact( etap ,q):
 def etap_from_ao_contact(u_ao,q):
 	return u_ao/(1.+3./2./q)
 
-def find_beta(betas, morse_reduced_b2s, target_reduced_b2):
+def find_epsilon(epsilons, morse_reduced_b2s, target_reduced_b2):
 	pos = abs(pl.array(morse_reduced_b2s)-target_reduced_b2).argmin()
-	return betas[pos], pos
+	return epsilons[pos], pos
 # INPUT : 
 
 q = 0.17
 rho0= 25.
-sigma = 1
-
-epsilon = 1
-
+sigma = 1.
+epsilon = 1.
 Uao = [2.4, 4.] #well depths in kbT
-
 etap = [ etap_from_ao_contact(u,q) for u in Uao]
 
 ao_params = [[q,sigma,e] for e in etap]
 
-morse_params = [rho0,sigma,epsilon]
+
+def square_well(r, params):
+	sigma = params[0]
+	epsilon = params[1]
+	lmbd = params[2]
+	p = np.zeros(len(r))
+	p [r<(sigma*lmbd)] = -epsilon
+	p [r<=sigma]=np.inf
+	return p
 # compute the reduced B2 for morse for a range of temperatures
 
 reduced_B2s_morse = []
-betas = pl.linspace(0.5,4,1000)
-for beta in betas:	
+epsilons = pl.linspace(min(Uao)/2.,max(Uao)*2,10000)
+beta = 1.0
 
+print ("\n ... Starting Mapping ...\n")
+for epsilon in epsilons:	
+	morse_params = [rho0,sigma,epsilon]
 	B2_morse = second_virial(morse_cut,morse_params,beta)
 	effective_sigma_morse = effective_sigma(morse_cut, morse_params,beta)
 	reduced_B2_morse = reduced_second_virial(B2_morse, effective_sigma_morse)
@@ -85,23 +95,39 @@ for beta in betas:
 for ao_p in ao_params:
 	print ("====> Uao ",ao_contact(ao_p[-1],q))
 	# Compute the reduced B2 for AO
-	B2_ao = second_virial(ao_potential,ao_p,1)
-	effective_sigma_ao= effective_sigma(ao_potential, ao_p,1)
+	B2_ao = second_virial(ao_potential,ao_p,beta)
+	effective_sigma_ao= effective_sigma(ao_potential, ao_p,beta)
 	reduced_B2_ao = reduced_second_virial(B2_ao, effective_sigma_ao)
 
-	beta ,pos= find_beta(betas,reduced_B2s_morse, reduced_B2_ao)
-	print ("Morse BetaEpsilon", beta*epsilon, "T", 1./beta)
+	eps ,pos= find_epsilon(epsilons,reduced_B2s_morse, reduced_B2_ao)
+	print ("*** Morse BetaEpsilon", beta*eps)
+	print ("")
 	print ("AO reduced B2", reduced_B2_ao)
 	print ("Morse reduced B2",reduced_B2s_morse[pos] )
 	print ("Morse effective sigma ",effective_sigma(morse_cut, morse_params,beta) )
+	r = pl.linspace(0.5,1.5,1000)
+	print ("Min AO", min(ao_potential(r,ao_p)))
+	print ("")
 
-	# pl.plot(betas, reduced_B2_ao*np.ones(len(betas)), '-k')
-	# pl.plot(betas, reduced_B2s_morse, '-')
+	# pl.plot(epsilons, reduced_B2_ao*np.ones(len(epsilons)), '-k')
+	# pl.plot(epsilons, reduced_B2s_morse, '-')
 	# pl.show()	
 
 	# plot the last potential
-	r = pl.linspace(0.5,1.5,1000)
-	pl.plot(r, ao_potential(r,ao_p))
-	pl.plot(r,beta*morse_cut(r, morse_params))
-	pl.ylim(np.floor(ao_potential(r,ao_params[1]).min())-1,10)
-	pl.show()
+	# r = pl.linspace(0.5,1.5,1000)
+	
+	# pl.plot(r, ao_potential(r,ao_p))
+	# pl.plot(r,beta*morse_cut(r, morse_params))
+	# pl.ylim(np.floor(ao_potential(r,ao_params[1]).min())-1,10)
+	# pl.show()
+
+print ("\n\nValidation with Square Well at Criticality:\nExpected tau = -1/(4 (B2_star -1)) = 0.0765")
+# Validation of the calculation:
+# check Noro and Frenkel J. Chem. Phys., Vol. 113, No. 8, 22 August 2000
+# table 1, for the Square  Well potential
+
+b2_sw = second_virial(square_well,[1,1,2],1/2.61)
+sigma_eff =  effective_sigma(square_well, [1,1,2],1/2.61)
+b2_star= reduced_second_virial(b2_sw, 1)
+tau = 1./(-4*(b2_star-1))
+print ("\n*** Found : B2_star", b2_star,"tau",tau)
