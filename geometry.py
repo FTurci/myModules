d366c4a179717ff81f80c96ee1c0cdbbca24fded
Francesco Turci <f.turci@bristol.ac.uk>
2018-10-30 14:57:50 +0000

    pbcpdist

--- geometry.py
+++ geometry.py
@@ -23,6 +23,7 @@ def pbcpdist(xyz, N, box):
                 d += dx**2
 
             values [count] = np.sqrt(d)
+            count +=1
     return values
 
 def rand_rotation_matrix3d(deflection=1.0, randnums=None):
