5416760e3aa46fe63696b73415c9ccba7ba21986
Francesco Turci <f.turci@bristol.ac.uk>
2019-01-25 14:42:11 +0000

    minor

--- pronto.py
+++ pronto.py
@@ -18,16 +18,19 @@ def decompress(filename,mode='rb',delete=False):
     with gzip.open(filename, mode) as fin:
       data = fin.read()
       filenameout = filename+".decompressed"
-      with open(filenameout ,'wb') as fout:
-        fout.write(data)
-      return filenameout
+      if os.path.isfile(filenameout):
+        print("!!! Skipping decompression: file already exists.")
+        return filenameout
+      else:
+        with open(filenameout ,'wb') as fout:
+          fout.write(data)
+        return filenameout
   else:
     print ("!!! Decompression failed: only .gz files are supported.")
 
 def clean(filename,extension = ".decompressed"):
   import os
   os.system("rm "+filename+extension)
-
 def cast_to_array(a):
   b = tuple([np.array(i) for i in a])
   return b
