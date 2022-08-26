a1a70dd824231e0107944389452b2df462320fd8
Francesco Turci <turci@me.com>
2022-08-26 14:45:13 +0100

    test

--- stringato.py
+++ stringato.py
@@ -10,8 +10,9 @@ def num_word(word,words):
     """Returns the first number after a certain word in a string of words."""
     # try float
     floats = re.compile(f'{word}'+'(\d+\.\d+)').findall(words)
+    print(word, floats)
     if len(floats[0])==0:
         ints = re.compile(f'{word}'+'(\d+)').findall(words)
-        return float(ints[0])
+        return int(ints[0])
     else:
         return float(floats[0])
